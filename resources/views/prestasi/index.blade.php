995cd49a34f02f2e882e5d3d96637d1921c012af
widad-sksn <indofals12345@gmail.com>
2026-07-14 17:53:10 +0700

    Fix error rendering achievements caused by accessing non-existent date field

--- resources/views/prestasi/index.blade.php
+++ resources/views/prestasi/index.blade.php
@@ -38,7 +38,7 @@
                     @endif
                     <div class="flex items-center text-sm font-medium text-gray-500">
                         <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
-                        Tahun {{ $achievement->date->format('Y') }}
+                        Tahun {{ $achievement->year }}
                     </div>
                 </div>
             </div>
